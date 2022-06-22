0a35f2ee68291726449d62480bf17adad8bb0cb7
betascribbles <firdaushassan033@gmail.com>
2022-06-22 20:01:12 +0300

    started a new react project - with template

---
+++ twitter_clone/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // BEM naming convention
+    return (
+      <div className="app">
+        <h1>twitter clone in progress</h1>
+
+        {/* Side bar */}
+
+        {/* Feed */}
+
+        {/* Widgets */}
+      </div>
+    );
+  }
+}
+
+export default App;
